8d906ad9b0d1bd7b98a7fcba932b74c906bdb5d5
Alessandro Hecht <alessandrohc@gmail.com>
2012-03-15 15:52:37 -0300

    Mantendo compatibilidade entre projetos django

--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
 # Django settings for {{ project_name }} project.
 import os
 
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-ROOT_DIR = os.path.split(PROJECT_DIR)[0]
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +10,16 @@ ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
+VERSION = "1.0 Beta 1"
+
+GRAPPELLI_ADMIN_TITLE = "Django Survey"
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR+'/examples.db',     # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH,'examples.db'),     # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -46,25 +49,52 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-LOCALE_PATHS=(os.path.join(ROOT_DIR,"survey"),)
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = PROJECT_DIR + '/site_media/'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'site_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/site_media/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
-SESSION_COOKIE_NAME = 'sessionid.survey'
+SECRET_KEY = 'sr7%j5-*fa4mq#be52hn45!2bym-f70_yp6n-*pznk^^'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -74,32 +104,42 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
 )
-
-ROOT_URLCONF = 'Django-survey.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    PROJECT_DIR + '/templates/',
+    os.path.join(PROJECT_PATH, 'templates'),
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.i18n",
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request'
+)
+
+GRAPPELLI_INDEX_DASHBOARD = 'Django-survey.dashboard.CustomIndexDashboard'
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
-    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'grappelli.dashboard',
+    'grappelli',
+    'django.contrib.admin',    
     'Django-survey',
     'survey',
 )
+
+ROOT_URLCONF = 'Django-survey.urls'
 
 
 LOGIN_URL = "/login/"
