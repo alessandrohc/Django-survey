608be074220c002888c97f1dcd9fca0379ce6487
Alex Sandro <alex@frabricadigital.com.br>
2012-03-19 11:21:25 -0300

    adicionado funções show/hide, com o objetivo de inativar a caixa de controles.

--- static/js/control_hide.js
+++ static/js/control_hide.js
@@ -1,30 +1,33 @@
+// Desenvolvido por: Alex Sandro
 // 
 (function($) {
   // ----------------------------------------------------
   function is_valid() { // se a seleção é válida.
 	var bool = {"S":true,"R":true,"C":true};
-	return bool[ $("#id_qtype").val() ]
+	return bool[$("#id_qtype").val()]
   }
   OnInit = function() { // mostra ou oculta de forma instantanea.
 	if (is_valid()) {
 	  $('#choices-group').css({"opacity": 1.0});
+	  $('#choices-group').show();
 	} else {
-	  $('#choices-group').css({"opacity": 0.0});;
+	  $('#choices-group').css({"opacity": 0.0});
+	  $('#choices-group').hide();
 	}
   }
-  OnSelect = function() { // aplica o efeito de trasição show/hide.
+  OnSelect = function() { // aplica o efeito de transição show/hide.
 	if (is_valid()) {
+		$('#choices-group').show(); // show - permite visualizar a animação.
 		$('#choices-group').animate({
 		  opacity: 1.0,
 		}, 300, function() {
 		  // Animation complete.
 		});
-		
 	  } else {
 		$('#choices-group').animate({
 		   opacity: 0.0,
 		 }, 300, function() {
-		   // Animation complete.
+		  $(this).hide(); // hide - iniativa os controles.
 		 });
 	}
   }
