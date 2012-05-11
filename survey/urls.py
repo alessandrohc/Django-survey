222615767df8f522cf9c5da7bdd6114cfceba3f4
Alex Sandro <alex@frabricadigital.com.br>
2012-05-11 17:55:52 -0300

    removido url de referência para respostas de usuários.

--- survey/urls.py
+++ survey/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 # Survey setting
 from django.views.generic.simple import redirect_to
 from models import Survey
-from views import answers_list, answers_detail,\
+from views import answers_list, \
                 survey_detail, survey_edit, survey_add,\
                 editable_survey_list, survey_delete, survey_update,\
                 question_add, question_update,question_delete,\
@@ -29,31 +29,25 @@ urlpatterns = patterns('',
     
     url(r'^editable/$', editable_survey_list, name='surveys-editable'),
     url(r'^detail/(?P<survey_id>[-\w]+)/$', survey_detail, name='survey-detail'),
-    url(r'^answers/(?P<survey_id>[-\w]+)/$', answers_list, name='survey-results'),
     
-    url(r'^answers/(?P<survey_id>[-\w]+)/(?P<key>[a-fA-F0-9]{10,40})/$', 
-        answers_detail,  name='answers-detail'),
+    url(r'^answers/(?P<survey_id>[-\w]+)/$', answers_list, name='survey-results'),
     
     url(r'^edit/(?P<survey_slug>[-\w]+)/$', survey_edit, name='survey-edit'),
     url(r'^add/$', survey_add, name='survey-add'),
     url(r'^update/(?P<survey_slug>[-\w]+)/$', survey_update, name='survey-update'),
     url(r'^delete/(?P<survey_slug>[-\w]+)/$', survey_delete, name='survey-delete'),
+    
     url(r'^question/add/(?P<survey_slug>[-\w]+)/$', question_add,   name='question-add'),
-    
-    url(r'^question/update/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', 
-        question_update, name='question-update'),
-    
-    url(r'^question/delete/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', 
-        question_delete, name='question-delete'),
+    url(r'^question/update/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', question_update, name='question-update'),
+    url(r'^question/delete/(?P<survey_slug>[-\w]+)/(?P<question_id>\d+)/$', question_delete, name='question-delete'),
     
     url(r'^choice/add/(?P<question_id>\d+)/$', choice_add, name='choice-add'),
     url(r'^choice/update/(?P<question_id>\d+)/(?P<choice_id>\d+)/$', choice_update, name='choice-update'),
     url(r'^choice/delete/(?P<survey_slug>[-\w]+)/(?P<choice_id>\d+)/$', choice_delete, name='choice-delete'),
     
-    url(r'^admin/$', redirect_to, {'url':'../../admin'}),
-    
     url(r'^api/$', 'survey.api.base'),
     url(r'^api/surveys$', 'survey.api.surveys'),
+    url(r'^admin/$', redirect_to, {'url':'../../admin'}),
     url(r'^api/surveys/(?P<survey_id>\d+)$', 'survey.api.surveys'),
     url(r'^api/surveys/(?P<survey_id>\d+).form$', 'survey.api.edit_survey'),
     url(r'^api/surveys/(?P<survey_id>\d+)/questions$', 'survey.api.questions'),
